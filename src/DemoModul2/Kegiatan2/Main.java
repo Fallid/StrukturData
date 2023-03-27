2383a4d944ddf0afcf0086323e2ca34217e9d4e0
Fallid <naufalopal002@gmail.com>
2023-03-27 11:45:55 +0700

    Praktikum Modul 2 Kegiatan 1 dan 2 Complit Java dan Python

--- src/DemoModul2/Kegiatan2/Main.java
+++ src/DemoModul2/Kegiatan2/Main.java
@@ -3,10 +3,13 @@ package DemoModul2.Kegiatan2;
 public class Main {
     public static void main(String[] args) {
         Link myLink = new Link();
-        myLink.add(6);
         myLink.add(8);
+        myLink.add(7);
+        myLink.add(1);
         myLink.add(4);
-        myLink.add(5);
+        myLink.add(6);
+        myLink.add(2);
+        myLink.add(3);
         
         //Linklist sebelum data di sorting
         System.out.print("Before : ");
