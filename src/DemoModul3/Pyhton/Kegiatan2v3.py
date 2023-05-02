3a11937e2a3179593227bb71399c778fd141dfd5
Fallid <naufalopal002@gmail.com>
2023-05-02 15:32:37 +0700

    Kegiatan 2 modul 3 Python done

--- src/DemoModul3/Pyhton/Kegiatan2v3.py
+++ src/DemoModul3/Pyhton/Kegiatan2v3.py
@@ -81,7 +81,7 @@ if __name__ == '__main__':
             
             print("Harga Barang\t: ", end="\b")
             inputHarga = input()
-            hargaBarang.EnQueue(inputHarga)
+            hargaBarang.EnQueue("Rp."+inputHarga)
             
             print("\nData Berhasil Ditambahkan! \nTekan apapun untuk kembali ke halaman utama...")
             input()
